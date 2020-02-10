bc7995995b38e57db3da746d3e910d3c5fe0c38c
tanya14109 <tanya14109@iiitd.ac.in>
2020-02-10 12:26:18 -0500

    Prim test

--- AI_683/HW1/Q6/Prims.py
+++ AI_683/HW1/Q6/Prims.py
@@ -9,35 +9,41 @@ class Prims:
 		self.grid_distances = distances
 		self.mst_vertices = numpy.zeros((N))
 		self.mst_parents = numpy.zeros((N))
-		self.mst_node_values = numpy.array([0] + [sys.maxsize]*(N-1))
+		self.mst_node_values = numpy.array([0] + [sys.maxsize]*(N-1),numpy.float32)
 		
-		self.mst_vertices[0]=-1
+		self.mst_parents[0]=-1
 		self.result = []
 		self.cost = 0
-
+		print(self.grid_distances,"\n\n",self.mst_vertices,"\n\n",self.mst_parents,"\n\n",self.mst_node_values)
 
 	def find_min_cutedge(self):
 		min_cutedge = sys.maxsize
 		min_cutindex = -1
 
 		for i in range(0,self.N):
-			if (self.mst_vertices[i]==0) & (min_cutedge >= self.mst_node_values[i]) :
+			if (self.mst_vertices[i]==0) & (min_cutedge > self.mst_node_values[i]) :
 				min_cutindex = i
 				min_cutedge = self.mst_node_values[i]
 
 		return min_cutindex 
 
 	def calculate_mst(self):
-		while(numpy.sum(self.mst_vertices) < self.N):
+		while numpy.sum(self.mst_vertices) < self.N :
 			node = self.find_min_cutedge()
+			#print("\n\nNode: ",node)
 			self.mst_vertices[node]=1
-			# self.cost+=self.mst_node_values[node]
+			self.cost+=self.mst_node_values[node]
+
+			#print("MST node values before: ",self.mst_node_values,"\n\n")
 
 			for child in range(0,self.N):
-				if ((self.mst_vertices[child]==0) & (self.grid_distances[node,child]<self.mst_node_values[child])) :
+				if ((node!=child) & (self.mst_vertices[child]==0) & (self.grid_distances[node,child]<self.mst_node_values[child])) :
 					self.mst_parents[child] = node
 					self.mst_node_values[child] = self.grid_distances[node,child]
 
-		print(self.mst_parents)
+			#print("MST node values after: ",self.mst_node_values,"\n\n")
+
+		print("MST Parents: ", self.mst_parents)
+		
 
 		return self.cost
--- tests/HW1/test_prims.py
+++ tests/HW1/test_prims.py
@@ -1,5 +1,6 @@
 # import pytest  # noqa
 import numpy as np
+import sys
 from AI_683.HW1.Q6.Prims import Prims
 from AI_683.HW1.Q6.problem_generator import run_main
 
@@ -7,7 +8,14 @@ def test_prims_runs():
 	grid_distances,N = run_main(6,0.4)	
 	p = Prims(N,grid_distances)
 	r = p.calculate_mst()	
-	print(r)
+	print("MST Cost", r)
 	# assert r == 25, "Failed r was {0}".format(r)
 
+def test_prims_simple():
+	grid_distances = np.array([[0,2,sys.maxsize,6,sys.maxsize],[2,0,3,8,5],[sys.maxsize,3,0,sys.maxsize,7],[6,8,sys.maxsize,0,9],[sys.maxsize,5,7,9,0]],np.float32)
+	p = Prims(5,grid_distances)
+	r = p.calculate_mst()	
+	print("MST Cost", r)
+
 test_prims_runs()
+test_prims_simple()
