845626063e25480466c419afc477c591391cbc8b
Abhinav Bhatia <bhatiaabhinav93@gmail.com>
2020-02-10 12:32:41 -0500

    code formatting

--- AI_683/HW1/Q6/Prims.py
+++ AI_683/HW1/Q6/Prims.py
@@ -3,48 +3,49 @@ import sys
 
 
 class Prims:
-	#N : number of vertices
-	#distance : 2D numpy array with euclidean distance between 2 nodes 
-	def __init__(self,N,distances):
-		self.N = N
-		self.grid_distances = distances
-		self.mst_vertices = numpy.zeros((N))
-		self.mst_parents = numpy.zeros((N))
-		self.mst_node_values = numpy.array([0] + [sys.maxsize]*(N-1),numpy.float32)
-		
-		self.mst_parents[0]=-1
-		self.result = []
-		self.cost = 0
-		print(self.grid_distances,"\n\n",self.mst_vertices,"\n\n",self.mst_parents,"\n\n",self.mst_node_values)
+    # N : number of vertices
+    # distance : 2D numpy array with euclidean distance between 2 nodes
+    def __init__(self, N, distances):
+        self.N = N
+        self.grid_distances = distances
+        self.mst_vertices = numpy.zeros((N))
+        self.mst_parents = numpy.zeros((N))
+        self.mst_node_values = numpy.array(
+            [0] + [sys.maxsize] * (N - 1), numpy.float32)
 
-	def find_min_cutedge(self):
-		min_cutedge = sys.maxsize
-		min_cutindex = -1
+        self.mst_parents[0] = -1
+        self.result = []
+        self.cost = 0
+        print(self.grid_distances, "\n\n", self.mst_vertices,
+              "\n\n", self.mst_parents, "\n\n", self.mst_node_values)
 
-		for i in range(0,self.N):
-			if (self.mst_vertices[i]==0) & (min_cutedge > self.mst_node_values[i]) :
-				min_cutindex = i
-				min_cutedge = self.mst_node_values[i]
+    def find_min_cutedge(self):
+        min_cutedge = sys.maxsize
+        min_cutindex = -1
 
-		return min_cutindex 
+        for i in range(0, self.N):
+            if (self.mst_vertices[i] == 0) & (min_cutedge > self.mst_node_values[i]):
+                min_cutindex = i
+                min_cutedge = self.mst_node_values[i]
 
-	def calculate_mst(self):
-		while numpy.sum(self.mst_vertices) < self.N :
-			node = self.find_min_cutedge()
-			#print("\n\nNode: ",node)
-			self.mst_vertices[node]=1
-			self.cost+=self.mst_node_values[node]
+        return min_cutindex
 
-			#print("MST node values before: ",self.mst_node_values,"\n\n")
+    def calculate_mst(self):
+        while numpy.sum(self.mst_vertices) < self.N:
+            node = self.find_min_cutedge()
+            # print("\n\nNode: ",node)
+            self.mst_vertices[node] = 1
+            self.cost += self.mst_node_values[node]
 
-			for child in range(0,self.N):
-				if ((node!=child) & (self.mst_vertices[child]==0) & (self.grid_distances[node,child]<self.mst_node_values[child])) :
-					self.mst_parents[child] = node
-					self.mst_node_values[child] = self.grid_distances[node,child]
+            # print("MST node values before: ",self.mst_node_values,"\n\n")
 
-			#print("MST node values after: ",self.mst_node_values,"\n\n")
+            for child in range(0, self.N):
+                if ((node != child) & (self.mst_vertices[child] == 0) & (self.grid_distances[node, child] < self.mst_node_values[child])):
+                    self.mst_parents[child] = node
+                    self.mst_node_values[child] = self.grid_distances[node, child]
 
-		print("MST Parents: ", self.mst_parents)
-		
+            # print("MST node values after: ",self.mst_node_values,"\n\n")
 
-		return self.cost
+        print("MST Parents: ", self.mst_parents)
+
+        return self.cost
