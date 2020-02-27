4a375a0abb4d30a14f1fe1c37576ff9160ea5a1f
tanya14109 <tanya14109@iiitd.ac.in>
2020-02-27 10:03:26 -0500

    Backtrack count added

--- AI_683/HW2/main.py
+++ AI_683/HW2/main.py
@@ -1,32 +1,39 @@
 from utils import read_puzzle, write_solution
 from constraints import global_constraints_check,all_assigned
 
-def backtracking(puzzle):
+def backtracking(puzzle, num_guesses):
 	assigned, (x,y) = all_assigned(puzzle)
 #	print (puzzle, (x,y))
 
 	if(assigned == True):
-		return True, puzzle
+		return True, puzzle, num_guesses
+
+	num_guesses+=8
 
 	for k in range(9):
 		puzzle[x][y]=str(k+1)
+	
 		if(global_constraints_check(puzzle) == True):
-			solve, puzzle = backtracking(puzzle)
+			solve, puzzle, num_guesses = backtracking(puzzle, num_guesses)
+	
 			if solve==True:
-				return True, puzzle
+				return True, puzzle, num_guesses
+	
 		puzzle[x][y]='-'
 
-	return False, puzzle
+	return False, puzzle, num_guesses
 
 
-puzzle_name = '100.txt'
+puzzle_name = '051.txt'
 puzzle = read_puzzle(puzzle_name)
 
 print("\n\nPuzzle: ")
 print(puzzle)
 
-solve, solution = backtracking(puzzle)
+solve, solution, num_guesses = backtracking(puzzle,0)
 #print(solve)
 print("\n\nSolution: ")
 
 print(solution)
+
+print(num_guesses)
