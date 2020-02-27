390fefca04816c42634fb3b5304fa3b0076bd0ee
tanya14109 <tanya14109@iiitd.ac.in>
2020-02-27 11:01:06 -0500

    MRV implemented

--- AI_683/HW2/main.py
+++ AI_683/HW2/main.py
@@ -1,8 +1,16 @@
-from utils import read_puzzle, write_solution
-from constraints import global_constraints_check,all_assigned
+from utils import read_puzzle, write_solution, get_domain_values, all_assigned,mrv_assigned
+from constraints import global_constraints_check
 
-def backtracking(puzzle, num_guesses):
-	assigned, (x,y) = all_assigned(puzzle)
+def forward_checking(puzzle,domain, x,y ):
+	assigned_val =  puzzle[x][y]
+	for i in range(9):
+		if i!=y:
+			domain[x][i].remove(assigned_val)
+		if i!=x:
+			domain[i][y].remove(assigned_val)
+
+def backtracking(puzzle,domain, num_guesses):
+	assigned, (x,y) = mrv_assigned(puzzle,domain)
 #	print (puzzle, (x,y))
 
 	if(assigned == True):
@@ -10,11 +18,11 @@ def backtracking(puzzle, num_guesses):
 
 	num_guesses+=8
 
-	for k in range(9):
-		puzzle[x][y]=str(k+1)
+	for element in domain[x][y]:
+		puzzle[x][y]= element
 	
 		if(global_constraints_check(puzzle) == True):
-			solve, puzzle, num_guesses = backtracking(puzzle, num_guesses)
+			solve, puzzle, num_guesses = backtracking(puzzle,domain, num_guesses)
 	
 			if solve==True:
 				return True, puzzle, num_guesses
@@ -24,16 +32,17 @@ def backtracking(puzzle, num_guesses):
 	return False, puzzle, num_guesses
 
 
-puzzle_name = '051.txt'
+puzzle_name = '100.txt'
 puzzle = read_puzzle(puzzle_name)
-
 print("\n\nPuzzle: ")
 print(puzzle)
 
-solve, solution, num_guesses = backtracking(puzzle,0)
-#print(solve)
+domain = get_domain_values(puzzle)
+# print("\n\nDomain: ")
+# print(domain)
+
+
+solve, solution, num_guesses = backtracking(puzzle,domain,0)
 print("\n\nSolution: ")
-
 print(solution)
-
-print(num_guesses)
+print(" \n\nNum Guesses:",num_guesses)
--- AI_683/HW2/utils.py
+++ AI_683/HW2/utils.py
@@ -11,6 +11,43 @@ def read_puzzle(puzzle_name):
 def write_solution(puzzle_name, puzzle):
 	numpy.savetxt('Solutions/'+puzzle_name, puzzle, delimiter=' ', fmt='%s')
 
+def get_domain_values(puzzle):
+	domain = numpy.empty((9,9),dtype=object)
+	for i in range(9):
+		for j in range(9):
+			if (puzzle[i][j]!='-') :
+				domain[i][j] = set(puzzle[i][j])
+			else:
+				domain[i][j] = set([str(k+1) for k in range(9) ])
+	return domain
+
+def all_assigned(puzzle):
+	for i in range(9):
+		for j in range(9):
+			if(puzzle[i][j]=='-'):
+				return False, (i,j)
+	return True, (9,9)
+	
+def mrv_assigned(puzzle, domain):
+	flag = True
+	min_rv = 99999
+	x,y = 9,9
+
+	for i in range(9):
+		for j in range(9):
+			if(puzzle[i][j]=='-'):
+				flag=False
+				if (len(domain[i][j])<min_rv):
+					x,y = i,j
+					min_rv = len(domain[i][j])
+
+	if flag==True:
+		return True, (9,9)
+	else:
+		return False, (x,y)
+
+
+
 # puzzle_name = '001.txt'
 # puzzle = read_puzzle(puzzle_name)
 # print(puzzle)
